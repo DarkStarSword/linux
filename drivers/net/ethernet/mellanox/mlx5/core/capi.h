c6423fd0a1028fba2ececa723933e88aa929aea9
Huy Nguyen <huyn@mellanox.com>
2016-06-18 10:16:56 -0500

    Merge branch 'cxl/mlx/cx4_rebasing' into cxl/mlx/cx4_rebasing_mlx
    Conflicts:
    	drivers/misc/cxl/pci.c
    	drivers/net/ethernet/mellanox/mlx5/core/main.c
    	include/linux/mlx5/driver.h

---
+++ drivers/net/ethernet/mellanox/mlx5/core/capi.h
@@ -0,0 +1,10 @@
+#ifdef CONFIG_MLX5_CAPI
+#include <linux/mlx5/driver.h>
+#include <misc/cxl.h>
+#include <linux/pci.h>
+
+int mlx5_capi_initialize(struct mlx5_core_dev *dev, struct pci_dev *pdev);
+int mlx5_capi_setup(struct mlx5_core_dev *dev, struct pci_dev *pdev);
+int mlx5_capi_cleanup(struct mlx5_core_dev *dev,
+		      struct pci_dev *pdev);
+#endif
