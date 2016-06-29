dbe7ad3563bf007acb14ce1da41251df62fdb971
Ian Munsie <imunsie@au1.ibm.com>
2016-06-29 16:28:06 +1000

    mlx5: Add support for interrupts in cxl mode
    In capi mode, interrupts must go through the XSL. The cxl driver
    transparently takes care of allocating the interrupts and configuring
    the XSL to deliver them, but the CX4 also requires the MSIX table to be
    filled out with a custom format to tell the card how to deliver them to
    the XSL.
    
    Additionally, the CX4 has a hardware limitation that restricts the
    maximum number of interrupts per capi context to 15, so this patch
    informs the cxl driver of that limitation so that it can allocate
    additional contexts as necessary.
    
    Signed-off-by: Ian Munsie <imunsie@au1.ibm.com>

--- drivers/net/ethernet/mellanox/mlx5/core/capi.h
+++ drivers/net/ethernet/mellanox/mlx5/core/capi.h
@@ -3,6 +3,7 @@
 #include <misc/cxl.h>
 #include <linux/pci.h>
 
+void mlx5_configure_msix_table_capi(struct pci_dev *pdev);
 int mlx5_capi_initialize(struct mlx5_core_dev *dev, struct pci_dev *pdev);
 int mlx5_capi_setup(struct mlx5_core_dev *dev, struct pci_dev *pdev);
 int mlx5_capi_cleanup(struct mlx5_core_dev *dev,
